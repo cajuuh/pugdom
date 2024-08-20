d08b25cde49d7ebadb65cfc632168776bbd2ffea
Pedro Alcantara <pedro.filipi.alcantara@gmail.com>
2024-08-20 16:36:59 -0300

    ⚡️ New button for the refresh in home screen
    the home screen now have a new button to refresh the timeline

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, forwardRef } from "react";
 import { FlatList, RefreshControl, ActivityIndicator } from "react-native";
 import { Container, WelcomeText } from "./styles/HomeScreen.style";
 import TootCard from "../../components/TootCard/TootCard";
@@ -6,59 +6,42 @@ import { useAppContext } from "../../context/AppContext";
 import { useTheme } from "@ui-kitten/components";
 import { useFeed } from "../../context/FeedContext";
 import { useIsFocused } from "@react-navigation/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import Banner, { BannerRef } from "../../components/Banner/Banner";
 
-const HomeScreen: React.FC = () => {
+const HomeScreen = forwardRef((props, ref) => {
   const isFocused = useIsFocused();
   const theme = useTheme();
   const { appParams } = useAppContext();
   const { username } = appParams;
-  const { feed, fetchFeed, checkForNewContent } = useFeed();
+  const { feed, fetchFeed, hasNewContent, setHasNewContent } = useFeed();
   const [refreshing, setRefreshing] = useState(false);
   const flatListRef = useRef<FlatList>(null);
   const bannerRef = useRef<BannerRef>(null);
-  const [autoUpdate, setAutoUpdate] = useState<boolean>(false);
 
   useEffect(() => {
-    // Load user preference
-    const loadPreference = async () => {
-      const storedPreference = await AsyncStorage.getItem(
-        "autoUpdatePreference"
-      );
-      setAutoUpdate(storedPreference === "true");
-    };
-    loadPreference();
+    fetchFeed();
   }, []);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (isFocused) {
-        checkForNewContent().then((hasNewContent) => {
-          if (hasNewContent) {
-            if (autoUpdate) {
-              fetchFeed(); // Automatically update the feed
-            } else {
-              bannerRef.current?.showBanner(); // Show banner notification
-            }
-          }
-        });
-      }
-    }, 120000); // Check every 2 minutes
-
-    return () => clearInterval(intervalId);
-  }, [checkForNewContent, isFocused, autoUpdate]);
-
-  const onRefresh = () => {
+  const onRefresh = async () => {
     setRefreshing(true);
-    fetchFeed().finally(() => setRefreshing(false));
+    await fetchFeed();
+    setRefreshing(false);
+    setHasNewContent(false);
+    bannerRef.current?.hideBanner();
   };
+
+  useEffect(() => {
+    if (hasNewContent) {
+      console.log("New content detected, showing banner...");
+      bannerRef.current?.showBanner();
+    }
+  }, [hasNewContent]);
 
   return (
     <Container theme={theme}>
       <WelcomeText theme={theme}>Welcome, {username}!</WelcomeText>
 
-      <Banner ref={bannerRef} onRefresh={onRefresh} />
+      {isFocused && <Banner ref={bannerRef} onRefresh={onRefresh} />}
 
       <FlatList
         ref={flatListRef}
@@ -88,6 +71,6 @@ const HomeScreen: React.FC = () => {
       />
     </Container>
   );
-};
+});
 
 export default HomeScreen;
