4fa074a36c5fbcd6598dd615dd67c15bdbb24b0a
Pedro Alcantara <pedro.filipi.alcantara@gmail.com>
2024-08-24 12:41:16 -0300

    💄 Add solid icons to app
    now lucide icons have it's two versions solid and not solid icons on the app

--- utils/Icons.tsx
+++ utils/Icons.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { SvgProps } from "react-native-svg";
 import { StyleProp, ViewStyle } from "react-native";
 import * as LucideIcons from "lucide-react-native";
+// Importing specific solid icons
+import { Heart as SolidHeart } from "lucide-solid"; // Import the specific solid icon you need
 
-export type IconName = keyof typeof LucideIcons;
+type OutlineIconName = keyof typeof LucideIcons;
 
 interface CustomIconProps {
-  name: IconName;
+  name: OutlineIconName | "Heart"; // Specify solid icons manually
+  solid?: boolean;
   color?: string;
   size?: number;
   style?: StyleProp<ViewStyle>;
@@ -14,11 +17,18 @@ interface CustomIconProps {
 
 const CustomIcon: React.FC<CustomIconProps> = ({
   name,
+  solid = false,
   color = "#000",
   size = 24,
   style,
 }) => {
-  const IconComponent = LucideIcons[name] as React.FC<SvgProps>;
+  let IconComponent;
+
+  if (solid && name === "Heart") {
+    IconComponent = SolidHeart as React.FC<SvgProps>;
+  } else {
+    IconComponent = LucideIcons[name as OutlineIconName] as React.FC<SvgProps>;
+  }
 
   if (!IconComponent) {
     return null;
