547fd73d36dfcf2d35e3bde9b8bfc06f5bcbcbb2
Pedro Alcantara <pedro.filipi.alcantara@gmail.com>
2024-08-25 00:06:27 -0300

    🍻 Change fonts
    the google fonts are now being applied

--- components/TootCard/styles/TootCard.style.tsx
+++ components/TootCard/styles/TootCard.style.tsx
@@ -4,15 +4,13 @@ import { Dimensions } from "react-native";
 const { width } = Dimensions.get("window");
 
 export const CardContainer = styled.View`
-  background-color: #fff;
   padding: 16px;
   margin: 8px 0;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   align-items: flex-start;
-  border-style: solid;
-  border-width: 2px;
-  border-color: #e6e6e6;
+  border-bottom-width: 2px;
+  border-bottom-color: #e6e6e6;
 `;
 
 export const ProfileImageContainer = styled.View`
@@ -61,7 +59,6 @@ export const MediaImage = styled.Image`
 `;
 
 export const ReblogContainer = styled.View`
-  background-color: #f0f0f0;
   padding: 10px;
   border-radius: 10px;
   width: ${width - 70}px;
@@ -70,8 +67,8 @@ export const ReblogContainer = styled.View`
 export const ReblogText = styled.Text`
   font-size: 14px;
   font-weight: bold;
-  margin-bottom: 5px;
   margin-left: 5px;
+  font-family: "PTSans_700Bold";
 `;
 
 export const SourceContainer = styled.View`
@@ -84,15 +81,17 @@ export const SourceUserContainer = styled.View`
   flex-direction: row;
   align-items: center;
   margin-top: 3px;
+  padding-vertical: 5px;
+  padding-horizontal: 5px;
+  border-radius: 25px;
+  background-color: #e6e9f2;
 `;
 
 export const SourceProfileImageContainer = styled.View`
   width: 20px;
   height: 20px;
   border-radius: 25px;
-  margin-right: 5px;
   margin-left: 5px;
-  margin-bottom: 5px;
   overflow: hidden;
 `;
 
@@ -104,6 +103,5 @@ export const SourceProfileImage = styled.Image`
 export const SourceUsername = styled.Text`
   font-size: 14px;
   font-weight: bold;
-  margin-bottom: 5px;
   margin-left: 5px;
 `;
